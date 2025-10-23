9ddbc183604ad6eb794d59b40794efa8e2c69145
Brave <132269905+Brave110705@users.noreply.github.com>
2025-10-23 20:14:58 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medicine Database</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-green-50 text-gray-800">
 
