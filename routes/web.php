1a0afeaf5a7c25ec90fcc60bb649e300dc4d786c
Brave <132269905+Brave110705@users.noreply.github.com>
2025-10-23 19:03:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MedicineController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     $isLoggedIn = Auth::check();
@@ -18,14 +19,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('medicines', MedicineController::class);
+    Route::post('/medicines/{id}/increase', [MedicineController::class, 'increaseStock'])->name('medicines.increase');
+    Route::post('/medicines/{id}/decrease', [MedicineController::class, 'decreaseStock'])->name('medicines.decrease');
 });
 
-Route::get('/medicines', [MedicineController::class, 'index'])->name('medicines.index');
-
-Route::resource('medicines', MedicineController::class)->middleware('auth');
-Route::post('/medicines/{id}/increase', [MedicineController::class, 'increaseStock'])->name('medicines.increase');
-Route::post('/medicines/{id}/decrease', [MedicineController::class, 'decreaseStock'])->name('medicines.decrease');
-Route::get('/medicines/{id}/edit', [App\Http\Controllers\MedicineController::class, 'edit'])->name('medicines.edit');
-Route::put('/medicines/{id}', [App\Http\Controllers\MedicineController::class, 'update'])->name('medicines.update');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
